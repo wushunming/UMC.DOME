ca420c6980b6bafe520438c99dbcc3b1cb1bae58
mingwu <service@365lu.cn>
2020-03-25 11:25:48 +0800

    上传
    UMC.DOME 快速开始DOME

---
+++ WebHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UMC.Demo
+{
+    public class WebHandler : System.Web.IHttpHandler
+    {
+
+        /// </summary>
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(System.Web.HttpContext context)
+        {
+
+            new Web.WebServlet().ProcessRequest(new WebContext(context));
+        }
+
+        #endregion
+    }
+}
